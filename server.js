5add33f3c54e9c8efd2f5a6b55b10f169e1be59c
BohemianHacks <jeff.blecha@gmail.com>
2019-05-12 00:13:59 -0500

    move session secret to environment variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const session = require('express-session');
 
 const app = express();
 
-app.use(session({secret: "poohbear"}));
+app.use(session({secret: process.env.SESSION_SECRET}));
 app.use(express.json());
 
 //routing
